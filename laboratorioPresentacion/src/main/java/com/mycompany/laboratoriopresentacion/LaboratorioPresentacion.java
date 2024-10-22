c52aaf42d8c6290341b7aa589d58645a3b914363
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-10-21 21:43:09 -0700

    Pantallas base
    Primeras pantallas
    
    - Fueron hechas por Orlando

---
+++ laboratorioPresentacion/src/main/java/com/mycompany/laboratoriopresentacion/LaboratorioPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratoriopresentacion;
+
+/**
+ *
+ * @author eduar
+ */
+public class LaboratorioPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
