e0b0a362b2aa8a6e5b6deae7b89701a664409a01
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-10-21 21:45:17 -0700

    Último proyecto
    - Con este tenemos todas las capas necesarias para trabajar

---
+++ laboratorioNegocio/src/main/java/com/mycompany/laboratorionegocio/LaboratorioNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorionegocio;
+
+/**
+ *
+ * @author eduar
+ */
+public class LaboratorioNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
