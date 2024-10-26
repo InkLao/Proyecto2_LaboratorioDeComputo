9c8599d6d618d18e88c03688ca9886fd622ecddb
ArturoITSON <sergio.garcia233316@potros.itson.edu.mx>
2024-10-26 00:17:50 -0700

    Pantalla Inicio e iniciar sesion estudiante y prestamoComputadoras

--- laboratorioPresentacion/src/main/java/pantallas/GestionComputadoras.java
+++ laboratorioPresentacion/src/main/java/pantallas/GestionComputadoras.java
@@ -24,7 +24,227 @@ private Administrador administrador;
     public GestionComputadoras(Administrador administrador) {
         this.administrador = administrador;
         initComponents();
+        
+        cargarMetodosIniciales();
+        
     }
+    
+    
+        private void cargarMetodosIniciales() {
+        this.cargarConfiguracionInicialTablaComputadoras();
+        this.cargarComputadorasEnTabla();
+
+    }
+    
+        
+    private void editarComputadoraTabla(ComputadoraDTO computadora) {
+        try {
+            System.out.println(bloqueo.toString() + "editar bloqueo id");
+            BloqueoDTO bloqueoActualizado = bloqueoNegocio.actualizarBloqueo(bloqueo);
+            System.out.println(bloqueoActualizado.getId());
+            JOptionPane.showMessageDialog(this, "Bloqueo editado");
+            this.cargarBloqueosEnTabla();
+            System.out.println(bloqueoActualizado.getId() + "22");
+
+            
+        } catch (NegocioException ex) {
+            JOptionPane.showMessageDialog(this, ex.getMessage(), "Información", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+    
+    
+    private void cargarConfiguracionInicialTablaBloqueos() {
+        ActionListener onEditarClickListener = new ActionListener() {
+            final int columnaId = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                editar();
+
+            }
+        };
+
+        int indiceColumnaEditar = 6;
+        TableColumnModel modeloColumnas = this.tblBloqueos.getColumnModel();
+        modeloColumnas.getColumn(indiceColumnaEditar)
+                .setCellRenderer(new JButtonRenderer("Editar"));
+        modeloColumnas.getColumn(indiceColumnaEditar)
+                .setCellEditor(new JButtonCellEditor("Editar",
+                        onEditarClickListener));
+
+        ActionListener onEliminarClickListener = new ActionListener() {
+            final int columnaId = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    //Metodo para eliminar un bloqueo
+                    eliminar();
+                } catch (NegocioException ex) {
+                    Logger.getLogger(GestionBloqueos.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+        int indiceColumnaEliminar = 7;
+        modeloColumnas = this.tblBloqueos.getColumnModel();
+        modeloColumnas.getColumn(indiceColumnaEliminar)
+                .setCellRenderer(new JButtonRenderer("Eliminar"));
+        modeloColumnas.getColumn(indiceColumnaEliminar)
+                .setCellEditor(new JButtonCellEditor("Eliminar",
+                        onEliminarClickListener));
+
+    }
+
+    private long getIdSeleccionadoTablaBloqueo() {
+        int indiceFilaSeleccionada = this.tblBloqueos.getSelectedRow();
+        if (indiceFilaSeleccionada != -1) {
+            DefaultTableModel modelo = (DefaultTableModel) this.tblBloqueos.getModel();
+            int indiceColumnaId = 0;
+            long idBloqueoSeleccionado = (long) modelo.getValueAt(indiceFilaSeleccionada,
+                    indiceColumnaId);
+            return idBloqueoSeleccionado;
+        } else {
+            return 0;
+        }
+    }
+
+    
+    private boolean getEliminadoSeleccionadoTablaBloqueo() {
+        int indiceFilaSeleccionada = this.tblBloqueos.getSelectedRow();
+        if (indiceFilaSeleccionada != -1) {
+            DefaultTableModel modelo = (DefaultTableModel) this.tblBloqueos.getModel();
+            int indiceColumnaId = 5;
+            boolean eliminadoBloqueoSeleccionado = (boolean) modelo.getValueAt(indiceFilaSeleccionada,
+                    indiceColumnaId);
+            return eliminadoBloqueoSeleccionado;
+        } else {
+            return false;
+        }
+    }
+    
+    private String getMotivoSeleccionadoTablaBloqueo() {
+        int indiceFilaSeleccionada = this.tblBloqueos.getSelectedRow();
+        if (indiceFilaSeleccionada != -1) {
+            DefaultTableModel modelo = (DefaultTableModel) this.tblBloqueos.getModel();
+            int indiceColumnaId = 1;
+            String motivoBloqueoSeleccionado = (String) modelo.getValueAt(indiceFilaSeleccionada,
+                    indiceColumnaId);
+            return motivoBloqueoSeleccionado;
+        } else {
+            return null;
+        }
+    }    
+
+
+    private void editar() {
+        //Metodo para regresar el alumno seleccionado
+
+        try{
+
+        BloqueoDTO bloqueo = new BloqueoDTO();
+
+        bloqueo = bloqueoNegocio.obtenerPorId(getIdSeleccionadoTablaBloqueo());
+
+        System.out.println(bloqueo.getId() + " si busco");
+        bloqueo.setMotivo(getMotivoSeleccionadoTablaBloqueo());
+        editarBloqueoTabla(bloqueo);
+        
+        cargarBloqueosEnTabla();
+        }
+        
+        catch(NegocioException e ){
+            e.printStackTrace();
+        }
+    }
+
+    private void eliminar() throws NegocioException {
+        //Metodo para regresar el beneficiario seleccionado
+        
+        BloqueoDTO eliminado = new BloqueoDTO();
+        
+        eliminado = bloqueoNegocio.obtenerPorId(getIdSeleccionadoTablaBloqueo());
+
+        System.out.println("Preparando el id: " + eliminado.getId() + " para borrar");
+        
+        eliminado.setEliminado(true);
+        
+        eliminarBloqueoTabla(eliminado);
+        cargarBloqueosEnTabla();
+
+    }
+
+    private void llenarTablaBloqueos(List<BloqueoDTO> bloqueosLista) {
+        DefaultTableModel modeloTabla = (DefaultTableModel) this.tblBloqueos.getModel();
+
+        if (modeloTabla.getRowCount() > 0) {
+            for (int i = modeloTabla.getRowCount() - 1; i > -1; i--) {
+                modeloTabla.removeRow(i);
+            }
+        }
+
+        if (bloqueosLista != null) {
+            bloqueosLista.forEach(row -> {
+                Object[] fila = new Object[6];
+                fila[0] = row.getId();
+                fila[1] = row.getMotivo();
+                fila[2] = row.getFechaBloqueo().getTime().toString();
+                fila[3] = row.getFechaLiberacion().getTime().toString();
+                fila[4] = row.getAlumno();
+                fila[5] = row.isEliminado();
+                modeloTabla.addRow(fila);
+            });
+        }
+    }
+
+    private void cargarBloqueosEnTabla() {
+        try {
+            List<BloqueoDTO> bloqueos = this.bloqueoNegocio.buscarBloqueosTabla();
+            this.llenarTablaBloqueos(bloqueos);
+        } catch (NegocioException ex) {
+            JOptionPane.showMessageDialog(this, ex.getMessage(), "Información", JOptionPane.ERROR_MESSAGE);
+        }
+    }    
+    
+    
+    private void cargarBloqueosEnTablaPorMotivo(String motivo) {
+        try {
+            
+            System.out.println(motivo);
+            List<BloqueoDTO> bloqueos = this.bloqueoNegocio.buscarBloqueosTabla(motivo);
+            
+            if(bloqueos.size() > 0){
+            
+            this.llenarTablaBloqueos(bloqueos);
+            
+            }
+            
+            else{
+               JOptionPane.showMessageDialog(this, "No se encontraron registros con los datos especificados, se mostraran todos los datos", "Informacion", JOptionPane.ERROR_MESSAGE); 
+                cargarBloqueosEnTabla();
+            }
+            
+            
+        } catch (NegocioException ex) {
+            JOptionPane.showMessageDialog(this, ex.getMessage(), "Información", JOptionPane.ERROR_MESSAGE);
+            cargarBloqueosEnTabla();
+        }
+    }         
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -44,6 +264,7 @@ private Administrador administrador;
         jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Gestionar Computadoras");
 
         jLabel1.setText("Gestion de Computadoras");
 
@@ -116,6 +337,7 @@ private Administrador administrador;
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
