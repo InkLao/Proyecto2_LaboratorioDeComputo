cfebfc83143a4983b6250a60d40dbab63793f021
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-10-27 06:26:12 -0700

    Reporte de Bloqueos
    - Es posible hacer un reporte de bloqueos hechos a alumnos
    - Los reportes deberían ser generados en cualquier máquina

--- laboratorioPersistencia/src/main/java/persistencia/BloqueoDAO.java
+++ laboratorioPersistencia/src/main/java/persistencia/BloqueoDAO.java
@@ -6,6 +6,7 @@ package persistencia;
 
 import Excepciones.PersistenciaException;
 import entidades.Bloqueo;
+import java.util.Calendar;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,20 +18,15 @@ import javax.persistence.TypedQuery;
  *
  * @author Oley
  */
-public class BloqueoDAO implements IBloqueoDAO{
-    
-    
+public class BloqueoDAO implements IBloqueoDAO {
+
     //private EntityManager entityManager;
-    
     private EntityManagerFactory emf = Persistence.createEntityManagerFactory("laboratorioComputo");
 
-
-    
     public BloqueoDAO(EntityManager entityManager, EntityManagerFactory emf) {
         //this.entityManager = entityManager;
     }
-    
-    
+
     @Override
     public List<Bloqueo> obtenerTodos() throws PersistenciaException {
         EntityManager em = emf.createEntityManager();
@@ -38,104 +34,112 @@ public class BloqueoDAO implements IBloqueoDAO{
         em.close();
         return bloqueos;
     }
-    
+
     @Override
     public Bloqueo agregarBloqueo(Bloqueo bloqueo) {
-        
+
         EntityManager entityManager = emf.createEntityManager();
 
         entityManager.getTransaction().begin();
         entityManager.persist(bloqueo);
         entityManager.getTransaction().commit();
         entityManager.close();
-        
+
         return bloqueo;
-    }  
-    
+    }
 
     @Override
     public Bloqueo editarBloqueo(Bloqueo bloqueo) {
 
         EntityManager entityManager = emf.createEntityManager();
 
-        
         System.out.println("bloqueoeditar " + bloqueo.toString());
         entityManager.getTransaction().begin();
         entityManager.merge(bloqueo);
         entityManager.getTransaction().commit();
         entityManager.close();
-        
+
         return bloqueo;
-        
+
     }
 
     @Override
-    public Bloqueo buscarBloqueo(Long id) throws PersistenciaException{
-        
-        try{
-        EntityManager entityManager = emf.createEntityManager();
-  
-        Bloqueo bloqueo = entityManager.find(Bloqueo.class, id);
-        
-        entityManager.close();
-        
-        return bloqueo;
-        }
-        
-        catch(Exception e){
+    public Bloqueo buscarBloqueo(Long id) throws PersistenciaException {
+
+        try {
+            EntityManager entityManager = emf.createEntityManager();
+
+            Bloqueo bloqueo = entityManager.find(Bloqueo.class, id);
+
+            entityManager.close();
+
+            return bloqueo;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("error en buscarBloqueo id persistencia");
         }
-        
+
         return null;
     }
 
-    
     @Override
-    public List<Bloqueo> buscarBloqueo(String motivo) throws PersistenciaException{
-       
-        try{
-        EntityManager em = emf.createEntityManager();
+    public List<Bloqueo> buscarBloqueo(String motivo) throws PersistenciaException {
 
-        String consultaJPQL = """
+        try {
+            EntityManager em = emf.createEntityManager();
+
+            String consultaJPQL = """
                                     SELECT b from Bloqueo b
                                     WHERE b.motivo = :motivo
                                 
                                 """;
             TypedQuery<Bloqueo> query = em.createQuery(consultaJPQL, Bloqueo.class);
             query.setParameter("motivo", motivo);
-            
+
             List<Bloqueo> bloqueo = query.getResultList();
-            
+
             return bloqueo;
-            
-        }
-        
-        catch(Exception e){
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-          
+
         return null;
     }
-    
+
     @Override
     public Bloqueo eliminarBloqueo(Bloqueo bloqueo) {
 
         EntityManager entityManager = emf.createEntityManager();
 
-        
         entityManager.getTransaction().begin();
         entityManager.merge(bloqueo);
         entityManager.getTransaction().commit();
         entityManager.close();
-        
-        
+
         return bloqueo;
-        
-        
-        
 
     }
-    
-    
+
+    @Override
+    public List<Bloqueo> buscarBloqueoPorFecha(Calendar fechaInicio, Calendar fechaFinal) throws PersistenciaException {
+        EntityManager em = emf.createEntityManager();
+        try {
+            String consultaJPQL = """
+                SELECT b FROM Bloqueo b
+                WHERE b.fechaBloqueo >= :fechaInicio AND b.fechaLiberacion <= :fechaFinal
+                """;
+
+            TypedQuery<Bloqueo> query = em.createQuery(consultaJPQL, Bloqueo.class);
+            query.setParameter("fechaInicio", fechaInicio);
+            query.setParameter("fechaFinal", fechaFinal);
+
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new PersistenciaException("Error al buscar bloqueos por fecha: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+    }
+
 }
