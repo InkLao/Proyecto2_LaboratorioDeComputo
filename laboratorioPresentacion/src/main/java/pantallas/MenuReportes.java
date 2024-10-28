90ea0c908ffa08519205bcf4738601a32bd73ed5
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-10-28 06:50:40 -0700

    Últimos aporte
    - Centrado de ventanas

--- laboratorioPresentacion/src/main/java/pantallas/MenuReportes.java
+++ laboratorioPresentacion/src/main/java/pantallas/MenuReportes.java
@@ -115,6 +115,7 @@ public class MenuReportes extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnRegresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegresarActionPerformed
--- laboratorioPresentacion/src/main/java/pantallas/ReporteCarrerasFiltro.java
+++ laboratorioPresentacion/src/main/java/pantallas/ReporteCarrerasFiltro.java
@@ -183,6 +183,7 @@ private ICarreraNegocio carreraNegocio;
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
